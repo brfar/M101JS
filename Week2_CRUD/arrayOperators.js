24763c676330322745f5c193f39f7a261d31d962
robnu <brobito@gmail.com>
2018-01-22 23:53:35 -0300

    ~

--- Week2_CRUD/arrayOperators.js
+++ Week2_CRUD/arrayOperators.js
@@ -1,43 +1,43 @@
 db.movieDetails
-	.find({
-		genres: {
-			$all: ['Comedy', 'Crime', 'Drama'] // Return movies which genres include Comedy AND Crime AND Drama
-		}
-	})
-	.pretty();
+  .find({
+    genres: {
+      $all: ['Comedy', 'Crime', 'Drama'] // Return movies which genres include Comedy AND Crime AND Drama
+    }
+  })
+  .pretty();
 
-///////////////////////////////////////
+//////////////////////////////////////
 
 db.movieDetails
-	.find({
-		countries: {
-			$size: 1 // Return movies which the array 'countries' only has one element
-		}
-	})
-	.pretty();
+  .find({
+    countries: {
+      $size: 1 // Return movies which the array 'countries' only has one element
+    }
+  })
+  .pretty();
 
-///////////////////////////////////////
+//////////////////////////////////////
 
 boxOffice: [
-	{ country: 'USA', revenue: 41.3 },
-	{ country: 'Australia', revenue: 2.9 },
-	{ country: 'UK', revenue: 10.1 },
-	{ country: 'Germany', revenue: 4.3 },
-	{ country: 'France', revenue: 3.5 }
+  { country: 'USA', revenue: 41.3 },
+  { country: 'Australia', revenue: 2.9 },
+  { country: 'UK', revenue: 10.1 },
+  { country: 'Germany', revenue: 4.3 },
+  { country: 'France', revenue: 3.5 }
 ];
 
 db.movieDetails.find({
-	'boxOffice.country': 'UK', // Would return movies from country UK
-	'boxOffice.revenue': {     // AND movies with revenue greater than 15. Not what we want.
-		$gt: 15
-	}
+  'boxOffice.country': 'UK', // Would return movies from country UK
+  'boxOffice.revenue': {     // AND movies with revenue greater than 15. Not what we want.
+    $gt: 15
+  }
 });
 
 db.movieDetails.find({
-	boxOffice: {
-		$elemMatch: {
-			country: 'UK', // Return movies from the UK in which the revenue is greater than 15!!
-			revenue: { $gt: 15 }
-		}
-	}
+  boxOffice: {
+    $elemMatch: {
+      country: 'UK', // Return movies from the UK in which the revenue is greater than 15!!
+      revenue: { $gt: 15 }
+    }
+  }
 });
